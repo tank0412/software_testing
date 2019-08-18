3f0f30b3ba3a2145e26ddd8d9251e0e5d26d1cf3
Roman <romabukhtiarov@gmail.com>
2019-08-18 15:42:27 +0300

    Impl work until user enter 'q'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,64 +4,65 @@ public class Main {
         System.out.println("Lab2");
         Input input = new Input();
         String inputWord = null;
-        do {
-            inputWord = input.requestWord("Please write a word");
-            System.out.println(inputWord);
-            if (input.checkUserInput(inputWord)) {
-                System.out.println("Input correct");
-            }
-        }
-        while(input.checkUserInput(inputWord) == false);
-        boolean checkIsExists = db.checkIsWordExists(inputWord);
-        if(checkIsExists) {
-            //Requests words with same root form db
-            String[] rootwords = db.getRootWords(inputWord);
-            for(int i = 0; i < rootwords.length; ++ i) {
-                if(rootwords[i] != null)
-                System.out.println(rootwords[i]);
-            }
-        }
-        else {
-            String[] wordParts = new String[3];
-            String record = input.requestWordForDB();
-            int j = 0, i;
-            int count = 0;
-            for(i = 0; i < record.length(); ++ i) {
-                char[] array = record.toCharArray();
-                if(array[i] == ' ' || i == array.length - 1) {
-                    int index = 0;
-                    char[] temp = new char[10];
-                    for(; j < i; ++j) {
-                        if(i == array.length - 1) ++i; // для последней буквы слова
-                        if(array[j]== ' ') continue;
-                        temp[index] = array[j];
-                        index++;
-                    }
-                    j=i;
-                    wordParts[count] = new String(temp);
-                    count++;
+        for(;;) {
+            do {
+                inputWord = input.requestWord("Please write a word");
+                //System.out.println(inputWord);
+                if (inputWord.equals("q")) return;
+                if (input.checkUserInput(inputWord)) {
+                    System.out.println("Input correct");
                 }
             }
-            //add new word to db
-            for(i = 0; i < 3; ++i) {
-                char[] tempCopy = wordParts[i].toCharArray();
-                int countLetters = 0;
-                for(j =0; j < tempCopy.length; ++j) {
-                    if(tempCopy[j] != 0) {
-                        countLetters++;
+            while (input.checkUserInput(inputWord) == false);
+            boolean checkIsExists = db.checkIsWordExists(inputWord);
+            if (checkIsExists) {
+                //Requests words with same root form db
+                String[] rootwords = db.getRootWords(inputWord);
+                for (int i = 0; i < rootwords.length; ++i) {
+                    if (rootwords[i] != null)
+                        System.out.println(rootwords[i]);
+                }
+            } else {
+                String[] wordParts = new String[3];
+                String record = input.requestWordForDB();
+                int j = 0, i;
+                int count = 0;
+                for (i = 0; i < record.length(); ++i) {
+                    char[] array = record.toCharArray();
+                    if (array[i] == ' ' || i == array.length - 1) {
+                        int index = 0;
+                        char[] temp = new char[10];
+                        for (; j < i; ++j) {
+                            if (i == array.length - 1) ++i; // для последней буквы слова
+                            if (array[j] == ' ') continue;
+                            temp[index] = array[j];
+                            index++;
+                        }
+                        j = i;
+                        wordParts[count] = new String(temp);
+                        count++;
                     }
                 }
-                char[] resized = new char[countLetters];
-                for(j =0; j < countLetters; ++j) {
-                    if(tempCopy[j] != 0) {
-                        resized[j] = tempCopy[j];
+                //add new word to db
+                for (i = 0; i < 3; ++i) {
+                    char[] tempCopy = wordParts[i].toCharArray();
+                    int countLetters = 0;
+                    for (j = 0; j < tempCopy.length; ++j) {
+                        if (tempCopy[j] != 0) {
+                            countLetters++;
+                        }
                     }
+                    char[] resized = new char[countLetters];
+                    for (j = 0; j < countLetters; ++j) {
+                        if (tempCopy[j] != 0) {
+                            resized[j] = tempCopy[j];
+                        }
+                    }
+                    wordParts[i] = new String(resized);
                 }
-                wordParts[i] = new String(resized);
-            }
-            db.insertWord(wordParts);
+                db.insertWord(wordParts);
 
+            }
         }
-        return;
     }
 }
