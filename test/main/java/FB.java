99836b2fd85f1e9fcfe0f6efcd62dd6953205203
Roman <romabukhtiarov@gmail.com>
2019-10-01 12:09:43 +0300

    Impl registration on Facebook

--- test/main/java/FB.java
+++ test/main/java/FB.java
@@ -5,8 +5,10 @@ import io.appium.java_client.android.ListensToLogcatMessages;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -43,6 +45,59 @@ public class FB {
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
     }
 
+    @Ignore
+    @Test
+    public void register() {
+        customWait();
+        driver.findElement(By.id("com.facebook.katana:id/login_create_account_button")).click();
+        driver.findElement(By.id("com.facebook.katana:id/finish_button")).click();
+        /*
+        try {
+            driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click(); // It ask for permissions during first sign up only
+            driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+        }
+        catch(NoSuchElementException exp) {
+            System.out.println("We have an exception " + exp );
+        }
+        */
+
+        MobileElement el1 = (MobileElement) driver.findElementById("com.facebook.katana:id/first_name_input");
+        el1.sendKeys("Ivan");
+        MobileElement el2 = (MobileElement) driver.findElementById("com.facebook.katana:id/last_name_input");
+        el2.sendKeys("Ivanov");
+        driver.findElement(By.id("com.facebook.katana:id/finish_button")).click();
+
+        driver.findElement(By.id("com.facebook.katana:id/finish_button")).click();
+        driver.findElement(By.id("com.facebook.katana:id/button1")).click(); // Yes, we sure that birthday is Oct 1 2001
+
+        driver.findElement(By.id("com.facebook.katana:id/gender_male")).click();
+        driver.findElement(By.id("com.facebook.katana:id/finish_button")).click();
+
+        driver.findElement(By.id("com.facebook.katana:id/switch_to_email_button")).click();
+
+        MobileElement emailInput = (MobileElement) driver.findElementById("com.facebook.katana:id/email_input");
+        emailInput.sendKeys("adasddfsadaffdhfh@yandex.com");
+        driver.findElement(By.id("com.facebook.katana:id/finish_button")).click();
+
+        MobileElement pwInput = (MobileElement) driver.findElementById("com.facebook.katana:id/password_input");
+        pwInput.sendKeys("TystusO4523");
+        driver.findElement(By.id("com.facebook.katana:id/finish_button")).click();
+
+        driver.findElement(By.id("com.facebook.katana:id/finish_without_contacts")).click();
+
+        /*
+
+        driver.findElement(By.id("com.facebook.katana:id/button1")).click(); // confirm identity if something unusual
+
+        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Continue \"]")).click();
+        */ // Impossible because it will ask a phone
+
+
+
+
+
+    }
+
     @Test
     public void login() {
         customWait();
@@ -52,14 +107,12 @@ public class FB {
         MobileElement el2 = (MobileElement) driver.findElementById("com.facebook.katana:id/login_password");
         el2.sendKeys("romanIv0125");
 
-    
         driver.findElement(By.id("com.facebook.katana:id/login_login")).click();
-
-        //driver.resetApp();
 
 
 
     }
+
 
     @After
     public void tearDown() {
