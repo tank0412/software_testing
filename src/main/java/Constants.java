c71b2443166f7eb0b26f7d76ce757c3796f99192
Roman <romabukhtiarov@gmail.com>
2019-10-02 20:51:03 +0300

    Move constants to interface

---
+++ src/main/java/Constants.java
@@ -0,0 +1,33 @@
+public interface Constants {
+    String tumblrUrl = "http://www.tumblr.com/";
+    String postInterractUrl = "http://komanda.tumblr.com/post/186855567115/%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82-%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%8C-%D1%82%D0%B5%D0%BF%D0%B5%D1%80%D1%8C-%D0%B2%D1%8B-%D0%BC%D0%BE%D0%B6%D0%B5%D1%82%D0%B5";
+    String tumblrDashboardUrl = "https://www.tumblr.com/dashboard";
+    String songName = "Кукушка гагарина";
+    String videoUrl = "https://youtu.be/Ra0ozaE-oy0";
+    String followAccountUrl = "https://fireflysheart.tumblr.com/";
+    String text = "Test";
+    String imageUrl = "D:/logoITMO_rus.png";
+    String quoteText = "Главная проблема цитат в интернете в том, что люди сразу верят в их подлинность";
+    String quoteAuthor = "Джейсон Стейтем";
+    String postUrl = "google.ru";
+    String chat = "деплоймент чат\n" +
+            "\n" +
+            "витя: коллеги, у нас на вечер запланирован релиз. нам нужно от вас ответственное лицо, чтобы кто-то мог нас подстраховать если что.\n" +
+            "Алексей Егоров: Если что?\n" +
+            "витя: если что-то пойдет не так.\n" +
+            "Алексей Егоров: Если что пойдёт не так?\n" +
+            "nikki: Коллеги, в самом деле, какого рода проблемы вы ожидаете? Релиз протестирован?\n" +
+            "витя: слушайте, протестирован. но если что-то будет с тестами, может потребоваться ваше содействие!\n" +
+            "Алексей Егоров: Если что будет с тестами?\n" +
+            "\n" +
+            "продакшн чат\n" +
+            "\n" +
+            "maxpatrol: отличный бот\n" +
+            "nikki: да\n" +
+            "zoppo2: мне кажется, они начнут догадываться, когда Алексей Егоров не явится на третий подряд корпоратив\n" +
+            "maxpatrol: сомневаюсь\n" +
+            "Cthuthu: не при нашей текучке кадров\n" +
+            "stty: да\n" +
+            "nikki: да";
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.util.concurrent.TimeUnit;
 
-public class Main {
+public class Main implements Constants {
     private static WebDriver driver;
     public static void main(String[] args) {
         System.out.println("Hello World, Lab1!");
@@ -12,7 +12,7 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "/chromedriver_win32/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize() ;
-        driver.get("http://www.tumblr.com/");
+        driver.get(tumblrUrl);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
         TumblrFunc tumblrFunc = new TumblrFunc(driver);
         //register(); // When we do not have account
@@ -25,49 +25,31 @@ public class Main {
         driver.get("http://www.tumblr.com/getting_to_know_tumblr/");
         postRegister();
         */ // To test postRegister after login
-            tumblrFunc.interractWithPost("http://komanda.tumblr.com/post/186855567115/%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82-%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%8C-%D1%82%D0%B5%D0%BF%D0%B5%D1%80%D1%8C-%D0%B2%D1%8B-%D0%BC%D0%BE%D0%B6%D0%B5%D1%82%D0%B5");
+            tumblrFunc.interractWithPost(postInterractUrl);
 
             tumblrFunc.customWait();
-            driver.get("https://www.tumblr.com/dashboard");
-            tumblrFunc.postMusic("Кукушка гагарина");
+            driver.get(tumblrDashboardUrl);
+            tumblrFunc.postMusic(songName);
             tumblrFunc.customWait();
-            tumblrFunc.postVideo("https://youtu.be/Ra0ozaE-oy0");
-            tumblrFunc.customWait();
-
-            tumblrFunc.followAccount("https://fireflysheart.tumblr.com/");
+            tumblrFunc.postVideo(videoUrl);
             tumblrFunc.customWait();
 
+            tumblrFunc.followAccount(followAccountUrl);
+            tumblrFunc.customWait();
 
-            tumblrFunc.blockAccount("https://fireflysheart.tumblr.com/");
-            driver.get("https://www.tumblr.com/dashboard");
+
+            tumblrFunc.blockAccount(followAccountUrl);
+            driver.get(tumblrDashboardUrl);
             tumblrFunc.customWait();
-            tumblrFunc.postText("Test");
+            tumblrFunc.postText(text);
             tumblrFunc.customWait();
-            tumblrFunc.postImage("D:/logoITMO_rus.png");
+            tumblrFunc.postImage(imageUrl);
             tumblrFunc.customWait();
-            tumblrFunc.postQuote("Главная проблема цитат в интернете в том, что люди сразу верят в их подлинность", "Джейсон Стейтем" );
+            tumblrFunc.postQuote(quoteText, quoteAuthor );
             tumblrFunc.customWait();
-            tumblrFunc.postLink("google.ru");
+            tumblrFunc.postLink(postUrl);
             tumblrFunc.customWait();
-            tumblrFunc.postChat("деплоймент чат\n" +
-                    "\n" +
-                    "витя: коллеги, у нас на вечер запланирован релиз. нам нужно от вас ответственное лицо, чтобы кто-то мог нас подстраховать если что.\n" +
-                    "Алексей Егоров: Если что?\n" +
-                    "витя: если что-то пойдет не так.\n" +
-                    "Алексей Егоров: Если что пойдёт не так?\n" +
-                    "nikki: Коллеги, в самом деле, какого рода проблемы вы ожидаете? Релиз протестирован?\n" +
-                    "витя: слушайте, протестирован. но если что-то будет с тестами, может потребоваться ваше содействие!\n" +
-                    "Алексей Егоров: Если что будет с тестами?\n" +
-                    "\n" +
-                    "продакшн чат\n" +
-                    "\n" +
-                    "maxpatrol: отличный бот\n" +
-                    "nikki: да\n" +
-                    "zoppo2: мне кажется, они начнут догадываться, когда Алексей Егоров не явится на третий подряд корпоратив\n" +
-                    "maxpatrol: сомневаюсь\n" +
-                    "Cthuthu: не при нашей текучке кадров\n" +
-                    "stty: да\n" +
-                    "nikki: да");
+            tumblrFunc.postChat(chat);
         }
         catch(UnhandledAlertException e) {
             System.out.println("Exception" + e);
@@ -77,8 +59,8 @@ public class Main {
 
     static void afterTest(TumblrFunc tumblrFunc) {
         tumblrFunc.customWait();
-        tumblrFunc.unfollowAccount("https://fireflysheart.tumblr.com/");
+        tumblrFunc.unfollowAccount(followAccountUrl);
         tumblrFunc.customWait();
-        tumblrFunc.unlikePost("https://komanda.tumblr.com/post/186855567115/%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82-%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%8C-%D1%82%D0%B5%D0%BF%D0%B5%D1%80%D1%8C-%D0%B2%D1%8B-%D0%BC%D0%BE%D0%B6%D0%B5%D1%82%D0%B5");
+        tumblrFunc.unlikePost(postInterractUrl);
     }
 }
