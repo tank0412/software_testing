e4b00e4734a96f5430d52aa1b27dc56ace2917bd
Roman <romabukhtiarov@gmail.com>
2019-10-21 20:02:52 +0300

    Add description of testInputIsMatchPattern

--- tests/InputTest.java
+++ tests/InputTest.java
@@ -7,8 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class InputTest {
     Input myinput = new Input();
 
+    //Проверить введенное слово на наличие цифр, латиницы и спец. Символов.
     @Test
-    public void testInputIsMatchPatter() {
+    public void testInputIsMatchPattern() {
         assertEquals(true, myinput.checkUserInput("привет"), "It contains cyrillyc so it must pass");
         assertEquals(false, myinput.checkUserInput("010101"), "It contains digit so it must fail");
         assertEquals(false, myinput.checkUserInput("hello@"), "It contains special symbols so it must fail");
