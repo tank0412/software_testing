35a2ecb752e871cf5550096f44f0ecc119aea434
Roman <romabukhtiarov@gmail.com>
2019-10-21 17:01:07 +0300

    Impl test of special symbols

--- tests/InputTest.java
+++ tests/InputTest.java
@@ -6,16 +6,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
     Input myinput = new Input();
-    @Test
-    public void testInputOfStringTestWord() {
-        Scanner scanner = new Scanner("test");
-        String result = myinput.requestWord("Write word 'test' to test function", scanner);
-        assertEquals("test", result, "It is not a 'test' word. Test fail");
-    }
+
     @Test
     public void testInputIsMatchPatter() {
         assertEquals(true, myinput.checkUserInput("привет"), "It contains cyrillyc so it must pass");
         assertEquals(false, myinput.checkUserInput("010101"), "It contains digit so it must fail");
+        assertEquals(false, myinput.checkUserInput("hello@"), "It contains special symbols so it must fail");
         //assertEquals(false, myinput.checkUserInput("test"), "It contains latin so it must fail"); Add temp latin support for null in input
     }
 }
