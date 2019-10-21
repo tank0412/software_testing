484179a499f8bb2f4f613e352998d543e11ceec1
Roman <romabukhtiarov@gmail.com>
2019-10-21 20:25:52 +0300

    Impl testInputWordInAFormat

--- tests/InputTest.java
+++ tests/InputTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,5 +16,28 @@ class InputTest {
         assertEquals(false, myinput.checkUserInput("010101"), "It contains digit so it must fail");
         assertEquals(false, myinput.checkUserInput("hello@"), "It contains special symbols so it must fail");
         //assertEquals(false, myinput.checkUserInput("test"), "It contains latin so it must fail"); Add temp latin support for null in input
+    }
+    //Реализовать поддержку ввода слова пользователем в формате: предкоренная часть слова, корень и посткоренная часть
+    @Test
+    void testInputWordInAFormat() {
+        String inputedWord= "предутренний\r\n" + "пред утрен ний\r\n" + "q";
+        InputStream in = new ByteArrayInputStream(inputedWord.getBytes());
+        System.setIn(in);
+        java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(out));
+        Main.main(null);
+        System.setIn(System.in);
+        System.setOut(System.out);
+
+        String output = out.toString();
+//
+        if(output.contains("You have entered not Two words!")) {
+            fail("Failed testInputWordInAFormat");
+        }
+        DatabaseTest dbt = new DatabaseTest();
+        dbt.deleteWordFromDB("пред", "утрен", "ний");
+
+
+
     }
 }
